4588f6b4b76957dceb2943826ec0cce6cc59388d
Sussan Chamling <sussan.chamling@hazesoft.co>
2025-06-03 21:48:49 +0545

    refactor: send a notification

--- src/notifications/dispatcher.notification.ts
+++ src/notifications/dispatcher.notification.ts
@@ -3,14 +3,16 @@ import { MailNotificationChannel } from "./channels/mail.notification.channel";
 import { PushNotificationChannel } from "./channels/push.notification.channel";
 import { SmsNotificationChannel } from "./channels/sms.notification.channel";
 
-const channel: Record<ChannelType, NotificationChannel> = {
-    sms: new SmsNotificationChannel(),
-    email: new MailNotificationChannel(),
-    push: new PushNotificationChannel()
-};
+const createChannel = (channelName: ChannelType): NotificationChannel => {
+    switch (channelName) {
+        case 'email': return new MailNotificationChannel();
+        case 'sms': return new SmsNotificationChannel();
+        case 'push': return new PushNotificationChannel();
+    }
+}
 
 const sendNotification = async (payload: NotificationPayload, channelName: ChannelType) => {
-    await channel[channelName].send(payload);
+    await createChannel(channelName).send(payload);
 }
 
 export { sendNotification }
